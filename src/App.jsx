3c4324960dc3294ef2154a5aa361e04100e0b5de
ChathurangiTelexar <chathurangi@telexarsoftware.com>
2025-10-27 11:23:42 +0530

    feat: implement useData custom hook for fetching and managing user data✅

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import UserAuth from './Context/useContext';
 import Login from './Components/Login';
 import { UserActivity } from './Context/userActivity';
 import UserInput from './Components/userInput';
+import useData from './Hooks/useData';
+
 
 function App() {
+  const [data, setData] = useData("users");
+  console.log("Data in App component:", data);
+
   return (
     <>
-    {<UserAuth>
-     
-      <Login/>
+      <UserAuth>
+        <Login />
+      </UserAuth>
 
-      </UserAuth>}
-    {/* <BrowserRouter>
-    
-      <Routes>
-        <Route path="/" element={<UseRef />} />
-      </Routes>
-    </BrowserRouter> */}
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UseRef />} />
+        </Routes>
+      </BrowserRouter> */}
     </>
   );
 }
---
+++ src/Hooks/useData.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
+
+const useData = (Location) => {
+  const [data, setData] = useState(null);
+  const url = `https://jsonplaceholder.typicode.com/${Location}`;
+
+  const fetchData = async (getLocation=Location) => {
+      try {
+        const response = await axios.get(url);
+        const data = response.data;
+        if (data){
+          console.log("Fetched data:", data);
+          setData(data);
+        } else {
+          toast.error("No data found for the specified location.");
+          setData(null);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        toast.error("Failed to fetch data. Please try again later.");
+      }
+    };
+
+  useEffect(() => {
+   fetchData();
+
+   return () => {
+    fetchData();
+   }
+  }, [Location]);
+
+  return [data, setData];
+};
+
+export default useData;
